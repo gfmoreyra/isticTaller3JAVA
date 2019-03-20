dcacc4cb639c94265ac3c65ba71d33554fc83b35
gfmoreyra <48774512+gfmoreyra@users.noreply.github.com>
2019-03-20 19:59:44 -0300

    primer ejemplo de salida y con concatenacion
    primer ejemplo de salida y con concatenacion

---
+++ clase1 EntradaSalida/src/clase1/entradasalida/Clase1EntradaSalida.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1.entradasalida;
+
+/**
+ *
+ * @author PC
+ */
+public class Clase1EntradaSalida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        int edad;
+        edad = 33;
+        System.out.println(" su edad es "+ edad);
+        // TODO code application logic here
+    }
+    
+}
